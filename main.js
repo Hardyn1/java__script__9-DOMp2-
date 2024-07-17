9045422dda20322b50fdcda144c21b2e3078807b
Hardyn1 <amki.business@gmail.com>
2024-07-17 13:31:24 +0300

    lesson id DONE

--- main.js
+++ main.js
@@ -75,3 +75,121 @@ console.log(divv.classList.contains("div-red")); // true.
 
 // 4 МЕТОД REMOVE.
 divv.classList.remove("div-letter");
+
+// ВСПЛЫТИЕ.
+// Когда на элементе происходит событие, обработчик сначала срабатывает на нем, потом на его родителе,
+// потом на родителе родителя и так далее вверх по цепочке. До самого document.
+let divAscent = document.getElementById("div-ascent");
+let pAscent = document.getElementById("p");
+let ulAscent = document.getElementById("ul");
+let liAscent = document.getElementById("liAscent");
+// !!! Кликаем по li хотя повесили событие на div.
+// Тоесть тут мы тыкнули сначала на li и по цепочке событие пошло наверх ul, div.
+divAscent.addEventListener("click", function(event){
+    alert("hello");
+    console.log(this); // Так как вложены еще элементы, то this выводит тот элемент на котором висит обработчик.
+    //  this (Текущий элемент до которого дошло событие).
+    // Вывело div-ascent так можно узнать по какому элементу мы тыкнули.
+    console.log(event.target); // Выводит точный целевой элемент по которому произвели клик.
+    // !!! В ПАРАМЕТРЕ И НАЗВАНИЕ ПЕРЕМЕННОЙ НЕ ОБЯЗАТЕЛЬНО НАЗЫВАТЬ EVENT. МОЖНО КАК ХОЧЕШЬ. 
+});
+
+// EVENT.TARGET.
+// Event.target это целевой элемент, а именно это тот элемент который вызывает событие.
+// ТЕПЕРЬ ПОВЕСИМ ПРОСЛУШКА СОБЫТИЯ НА КАЖДЫЙ ИЗ ЭЛЕМЕНТОВ.
+
+// МЕТОД STOP.PROPOGATION.
+// По которому тыкнули по тому и споп дальше всплытие не пойдет, благодаря stop.propagation.
+// pAscent.addEventListener("click", function(){
+// console.log("События с p");
+// });
+
+// ulAscent.addEventListener("click", function(){
+// console.log("События с ul");
+// });
+
+// liAscent.addEventListener("click", function(events){
+//     events.stopPropagation();
+// console.log("События с li");
+// });
+
+// ДЕЛЕГИРОВАНИЕ СОБЫТИЙ.
+// Чтобы не вешать на каждый элемент по id, можно просто по клику определить и поставить событие.
+let cubeWrapper = document.getElementById("cube-wrapper");
+
+cubeWrapper.addEventListener("click", function(evt){
+    let target = evt.target;
+    target.classList.add("yellow");
+    console.log(target);
+});
+
+// PREVENT.DEFAULT().
+// Отменяет стандартные действия.
+let link = document.getElementById("link");
+link.onclick = function(evs) {
+evs.preventDefault(link);
+alert("click");
+};
+
+// ИНТЕРФЕЙСНЫЕ СОБЫТИЯ (МЫШИ).
+let divvs = document.getElementById("divvs");
+    divvsBtn = document.getElementById("divvs-btn");
+
+divvs.addEventListener(`contextmenu`, function(){
+    console.log("run");
+});
+
+let input = document.getElementById("input");
+
+input.addEventListener("keydown", function(){
+console.log("клавиша нажата"); //выводит клавиша нажата при каждом нажатии клавиши.
+});
+
+// КАК УЗНАТЬ КАКАЯ КЛАВИША НАЖАТА ?
+// Так можно узнать какие клавиши нажал пользователь.
+// event.key покажет что за клавиша нажата.
+// А event.code покажет какой код у клавиши.
+input.addEventListener("keydown", function(ets){
+    let key = ets.key;
+console.log(key);
+});
+
+input.addEventListener("keydown", function(ets){
+    let code = ets.code; // если нажал f = KeyF.
+console.log(code);
+});
+
+// ИНТЕРФЕЙСНЫЕ СОБЫТИЯ ФОРМЫ.
+let inputTxt = document.getElementById("input-txt");
+    textArea = document.getElementById("textarea");
+    checkbox = document.getElementById("checkbox");
+    formBtn = document.getElementById("form-btn");
+
+formBtn.addEventListener("click", function(e){
+    e.preventDefault();
+console.log(textArea.value); // Через свойство value мы получаем значение элементов(из тех мест куда мы что то записываем). 
+});
+
+checkbox.addEventListener("click", function(e){
+console.log(e.targed.checked); // Через свойство checked мы узнаем поставил ли пользователь галочку или нет. 
+});
+
+input.addEventListener("focus", function(e){
+    console.log("run");
+});
+
+input.addEventListener("blur", function(e){ // Blur чтобы делать какую то рамочку
+    console.log("run");
+});
+
+input.addEventListener("change", function(e){ // Событие Change срабатывает при любом окончании изменении.
+    console.log("run");
+});
+
+input.addEventListener("change", function(e){ // Событие input срабатывает при любой изменении и работе.
+    console.log("run");
+});
+
+input.addEventListener("copy", function(e){ // Событие copy срабатывает при ctrl + c.
+    console.log("run");
+});
--- style+offset/style_offset.js
+++ style+offset/style_offset.js
@@ -27,3 +27,55 @@ console.log(cube.offsetParent);
 
 // Свойства offsetLeft / offsetTop.
 // Эти свойства содержат координаты x/y относительно верхнего левого угла offsetParent.
+// !!! В ДАННЫЙ МОМЕНТ ДЛЯ ЭЛЕМЕНТОВ WRAPPER / CUBE offsetParent ЯВЛЯЕТСЯ BODY, ПОЭТОМУ ОТНОСИТЕЛЬНО BODY.
+console.log(wrapper.offsetTop); //414 (Расстояние до нашего body, включая margin).
+// Если убрать body margin: 0; то расстояние 406.
+console.log(wrapper.offsetLeft); //8.
+console.log(cube.offsetTop);
+console.log(cube.offsetLeft);
+
+// Свойства offsetWidth / offsetHeight.
+// Эти свойства содержат полный размер элемента.
+console.log(wrapper.offsetWidth); // 450 width (включая padding).
+console.log(wrapper.offsetHeight); // 450 height.
+console.log(cube.offsetWidth); // 200.
+console.log(cube.offsetHeight); // 200.
+
+// Свойства clientTop / clientLeft.
+// Эти свойства заработают если у нас будут рамки у элемента. Если же там будет еще полоса прокрутки,
+// то она также попадет в это свойство.
+console.log(wrapper.clientTop); // 0 (изначально, затем добавил border 10 px) и выводит 10.
+console.log(wrapper.clientLeft); // 0.
+
+// Свойства clientWidth / clientHeight.
+// Это свойства, размер области внутри рамок.
+console.log(wrapper.clientWidth); // 450 (показывает чистую ширину элемента без рамок, тоесть border).
+console.log(wrapper.clientHeight); // 450.
+
+
+// Свойства scrollWidth / scrollHeight.
+// Высота и ширина прокрутки.
+console.log(cube.scrollHeight); // 828 (Высота всего скролла в тексте).
+console.log(cube.scrollWidth); // 200 (Ширина всего скролла в тексте).
+
+// Свойства scrollLeft / scrollTop.
+// Высота прокрученной на данный момент части элемента.
+// ПОЛЬЗОВАТЕЛЬ ПРОКРУТИЛ ДО КУДА ТО НАЖАЛ НА КНОПКУ MOVE UP И НА САМОЕ НАЧАЛО СКРОЛЛА ВЕРНУЛСЯ.
+let cubeBtn = document.getElementById("cube-btn");
+cubeBtn.addEventListener("click", function(){
+cube.scrollTop = cube.scrollTop - cube.scrollTop;
+// cube.ScrollTop = текущий cube.scrollTop на котором остановились и вычесть саму себя cube.scrollTop.
+});
+
+console.log(cube.scrollLeft); // 0.
+console.log(cube.scrollTop);
+
+// Применим событие и функцию чтобы прике на полосу прокрутки,
+// чтобы у нас выдавало координаты прокрутки на данный момент.
+cube.addEventListener("scroll", function(){
+console.log(cube.scrollTop);
+});
+
+// pageYOffset получаем текущее значени нашего окна браузера.
+console.log(window.pageYOffset);
+
